1e8ffd5a5b4000fa9370a664e8318e212e809da1
santhisenan <santhisenan@outlook.com>
2017-08-10 08:09:00 +0530

    login done

--- app/models/user.js
+++ app/models/user.js
@@ -24,24 +24,37 @@ module.exports.createUser = function(newUser , callback){
 
 }
 
+
+
 passport.use(new LocalStrategy(function(username, password, done) {
   process.nextTick(function() {
     User.findOne({
       'username': username,
     }, function(err, user) {
+
       if (err) {
         return done(err);
       }
 
+
       if (!user) {
         return done(null, false);
       }
+      console.log("User exists");
 
-      if (user.password != password) {
-        return done(null, false);
-      }
+      bcrypt.compare(password, user.password, function(err, res) {
+        if(err) throw err;
+        if(res == false){
+          // console.log(res);
+          return done(null , false);
+        }
 
-      return done(null, user);
+        if(res === true){
+          return done(user , true)
+        }
+      });
+
+      // return done(null, user);
     });
   });
 }));
