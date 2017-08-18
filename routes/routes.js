6e59687ef3a235ec88591fff11326b6496b4b43c
santhisenan <santhisenan@outlook.com>
2017-08-18 17:02:08 +0530

    token generation done

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var passport = require('passport')
-// var LocalStrategy = require('passport-local').Strategy;
-// var bcrypt = require('bcryptjs')
 
 var User = require('../app/models/user')
 var router = express.Router();
@@ -15,63 +13,49 @@ router.get('/' , function(req , res){
 
 // '/users' route
 router.route('/users')
-.get(function(req,res){
-  User.find({} , function(err, users){
-    if(err)
-    console.log(err);
-    else{
-      res.send(users.toString());
+  .get(function(req,res){
+    User.find({} , function(err, users){
+      if(err)
+      console.log(err);
+      else{
+        res.send(users.toString());
+      }
+    });
+  })
+  .post(function(req , res){
+    var user = new User();
+    user.username = req.body.username;
+    user.password = req.body.password;
+    var callback = function(err , message){
+      if(err)
+      throw err;
+
+      if(message)
+      res.send(message)
+
+      if(user){
+        res.send({
+          message:"User created" , user:user
+        })
+      }
+
     }
+    User.createUser(user , callback);
   });
-})
-.post(function(req , res){
-  var user = new User();
-  user.username = req.body.username;
-  user.password = req.body.password;
-  // console.log(req.body);
-  var callback = function(err , message){
-    if(err)
-    throw err;
 
-    if(message)
-    res.send(message)
-
-    if(user)
-    res.send({
-      message:"User created" , user:user
-    })
+router.get('/isloggedin' , function(req,res,next){
+  if(req.user){
+    res.send({"message": req.user+" authenticated"})
+    next();
   }
-  User.createUser(user , callback);
-});
-
+  else
+    res.send({"message": "not authenticated"})
+})
 
 //login route
 router.post('/login' , function(req, res, next){
   User.verifyCredentials(req, res, next);
 });
-
-// router.post('/login',
-//   passport.authenticate('local'),
-//   function(req, res) {
-//     console.log("authenticated");
-//     // If this function gets called, authentication was successful.
-//     // `req.user` contains the authenticated user.
-//     if(req.user){
-//       // console.log(req.user);
-//       // User.generateToken(req , res);
-//       res.send({"message":"Authenticated successfully"})
-//     }
-//     else
-//       res.send({"message": "Invaid username/password"})
-//   });
-
-
-router.get('/loginSuccess' , function(req, res, next){
-  res.send({message:"Authentication Success"})
-})
-router.get('/loginFailure' , function(req, res, next){
-  res.send({message:"Authentication Failed !!"})
-})
 
 
 module.exports = router;
