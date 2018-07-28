319ca4ff5332298d6ae3b1a57b7e04eaed96a22d
Bilal <bilaljaleel123@gmail.com>
2018-07-28 12:52:46 +0530

    Updated Server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.use(passport.initialize())
 
 // set Headers
 app.use(cors({
-  origin: [ 'http://localhost:3000', 'http://bloodcellcet.ml' ],
+  origin: [ 'http://localhost:8080', 'http://bloodcellcet.ml' ],
   credentials: true
 }))
 
