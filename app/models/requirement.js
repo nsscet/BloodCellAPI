8c0b7b4675bf42ebc3c51a90bd97d61a4e54531d
Bilal <bilaljaleel123@gmail.com>
2018-09-09 21:37:46 +0530

    Link and delink api

--- app/models/requirement.js
+++ app/models/requirement.js
@@ -1,74 +1,99 @@
-
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
-
-var requirementSchema = new mongoose.Schema(
-  {
+var childSchema = new Schema({
+    donorId: {
+        type: String
+    }
+})
+var requirementSchema = new mongoose.Schema({
     hospitalId: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
     bloodGroup: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
     quantity: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
     isClosed: {
-      type: Number,
-      default: 0
+        type: Number,
+        default: 0
     },
     timeOfPosting: {
-      type: Date,
-      default: Date.now()
+        type: Date,
+        default: Date.now()
     },
     typeOfRequirement: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
     patientId: {
-      type: String,
-      required: true
+        type: String,
+        required: true
     },
-    contactNo:{
-      type:Number,
-      required: true
+    contactNo: {
+        type: Number,
+        required: true
     },
-    remarks:{
-      type:String
-    }
-  }
-)
+    remarks: {
+        type: String
+    },
+    children: [childSchema]
+}, { usePushEach: true })
 
 var Requirement = module.exports = mongoose.model('Requirement', requirementSchema)
 
 module.exports.getRequirements = (query, callback) => {
-  Requirement.find(query, function (err, requirements) {
-    if (err) {
-      callback(err, null)
-    } else {
-      callback(null, requirements)
-    }
-  })
+    console.log(query)
+    Requirement.find(query, function(err, requirements) {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, requirements)
+        }
+    })
 }
 
 module.exports.addRequirement = (requirement, callback) => {
-  requirement.save((err) => {
-    if (err) {
-      callback(err, null)
-    } else {
-      callback(null, requirement)
-    }
-  })
+    requirement.save((err) => {
+        if (err) {
+            callback(err, null)
+        } else {
+            callback(null, requirement)
+        }
+    })
 }
 module.exports.updateRequirement = (query, updatedValue, callback) => {
-  Requirement.updateOne(query, updatedValue, (err, res) => {
-    if (err) {
-      callback(err)
-    } else {
-      callback(err)
-    }
-  })
+    Requirement.updateOne(query, updatedValue, (err, res) => {
+        console.log(err)
+        if (err) {
+            callback(err)
+        } else {
+            callback(err)
+        }
+    })
+}
+module.exports.linkDonor = (query, value, callback) => {
+    Requirement.findOne(query, function(err, requirement) {
+        try {
+
+            requirement.children.push(value)
+            requirement.save((err) => {
+                console.log(err)
+            })
+
+        } catch (error) {
+            console.log("Error:" + error)
+            callback(error)
+        }
+        callback(false)
+    })
+
+
+}
+module.exports.deLinkDonor = (query, donorId, callback) => {
+    Requirement.update(query, { "$pull": { children: { "donorId": donorId } } }, callback)
 }
--- routes/requirement.js
+++ routes/requirement.js
@@ -5,95 +5,139 @@ var allowAccess = require('../app/middleware/allowAccess')
 var Requirement = require('../app/models/requirement')
 
 router.use(
-  function (req, res, next) {
-    verifyToken(req, res, next)
-    allowAccess(req, res, next, 'requirement')
-  })
+    function(req, res, next) {
+        verifyToken(req, res, next)
+        allowAccess(req, res, next, 'requirement')
+    })
 
 router.route('/requirements')
-  .get(function (req, res) {
-    var callback = (err, requirements) => {
-      if (err) {
-        res.send({
-          success: false,
-          message: 'Cannot get results. Try again'
-        })
-      } else {
-        if (requirements.length > 0) {
-          res.send({
-            count: requirements.length,
-            success: true,
-            requirements,
-            message: 'Requirements successfully returned'
-          })
-        } else {
-          res.send({
-            count: requirements.length,
-            success: false,
-            message: 'No requirements found'
-          })
+    .get(function(req, res) {
+        var callback = (err, requirements) => {
+            if (err) {
+                res.send({
+                    success: false,
+                    message: 'Cannot get results. Try again'
+                })
+            } else {
+                if (requirements.length > 0) {
+                    res.send({
+                        count: requirements.length,
+                        success: true,
+                        requirements,
+                        message: 'Requirements successfully returned'
+                    })
+                } else {
+                    res.send({
+                        count: requirements.length,
+                        success: false,
+                        message: 'No requirements found'
+                    })
+                }
+            }
         }
-      }
-    }
-    Requirement.getRequirements(req.query, callback)
-  })
-  .post((req, res) => {
-    if(!(req.session.user.role === 'sadmin' || req.session.user.role === 'hospitals')){
-      return res.status(304).send({
-        message:'not authorised'
-      })
-    }
-    var requirement = new Requirement()
-    requirement.hospitalId = req.body.hospitalId
-    requirement.bloodGroup = req.body.bloodGroup
-    requirement.quantity = req.body.quantity
-    requirement.typeOfRequirement = req.body.typeOfRequirement
-    requirement.patientId = req.body.patientId
-    requirement.remarks = req.body.remarks
-    requirement.contactNo = req.body.contactNo
-    var callback = (err, requirement) => {
-      if (err) {
-        throw err
-        res.send({
-          success: false,
-          message: 'Some error occured'
-        })
-      } else {
-        res.send({
-          success: true,
-          message: 'Requirement successfully created',
-          requirement
-        })
-      }
-    }
-    Requirement.addRequirement(requirement, callback)
-  }).put((req, res) => {
-    console.log(req.body)
-    var query = {hospitalId: req.body.hospitalId,
-      bloodGroup: req.body.bloodGroup,
-      typeOfRequirement: req.body.typeOfRequirement,
-      patientId: req.body.patientId,
-      timeOfPosting: req.body.timeOfPosting,
-      contactNo:req.body.contactNo
-    }
-    var updatedValue = { $set: {isClosed: req.body.isClosed,
-                                quantity: req.body.quantity,
-                                remarks:req.body.remarks}
-                        }
-    var callback = (err) => {
-      if (err) {
-        res.send({
-          success: false,
-          message: 'Some error occured'
-        })
-      } else {
-        res.send({
-          success: true,
-          message: 'Requirement Updated'
-        })
-      }
-    }
-    Requirement.updateRequirement(query, updatedValue, callback)
-  })
+        Requirement.getRequirements(req.query, callback)
+    })
+    .post((req, res) => {
+        if (!(req.session.user.role === 'sadmin' || req.session.user.role === 'hospitals')) {
+            return res.status(304).send({
+                message: 'not authorised'
+            })
+        }
+        var requirement = new Requirement()
+        requirement.hospitalId = req.body.hospitalId
+        requirement.bloodGroup = req.body.bloodGroup
+        requirement.quantity = req.body.quantity
+        requirement.typeOfRequirement = req.body.typeOfRequirement
+        requirement.patientId = req.body.patientId
+        requirement.remarks = req.body.remarks
+        requirement.contactNo = req.body.contactNo
+        var callback = (err, requirement) => {
+            if (err) {
+                throw err
+                res.send({
+                    success: false,
+                    message: 'Some error occured'
+                })
+            } else {
+                res.send({
+                    success: true,
+                    message: 'Requirement successfully created',
+                    requirement
+                })
+            }
+        }
+        Requirement.addRequirement(requirement, callback)
+    }).put((req, res) => {
+        console.log(req.body)
+        var query = {
+            hospitalId: req.body.hospitalId,
+            bloodGroup: req.body.bloodGroup,
+            typeOfRequirement: req.body.typeOfRequirement,
+            patientId: req.body.patientId,
+            timeOfPosting: req.body.timeOfPosting,
+            contactNo: req.body.contactNo
+        }
+        var updatedValue = {
+            $set: {
+                isClosed: req.body.isClosed,
+                quantity: req.body.quantity,
+                remarks: req.body.remarks
+            }
+        }
+        var callback = (err) => {
+            if (err) {
+                res.send({
+                    success: false,
+                    message: 'Some error occured'
+                })
+            } else {
+                res.send({
+                    success: true,
+                    message: 'Requirement Updated'
+                })
+            }
+        }
+        Requirement.updateRequirement(query, updatedValue, callback)
+    })
+router.route('/requirements/:id')
+    .put((req, res) => {
+        var query = {
+            "hospitalId": req.body.hospitalId,
+            "bloodGroup": req.body.bloodGroup,
+            "typeOfRequirement": req.body.typeOfRequirement,
+            "patientId": req.body.patientId,
+            "contactNo": req.body.contactNo
+        }
+        var updatedValue = {
+            donorId: req.params.id
+        }
+        var callback = (err) => {
+            if (err) {
+                res.send({ "Status": false, "Error": err })
+            } else {
+                res.send({ "Status": true })
+            }
+        }
 
+        Requirement.linkDonor(query, updatedValue, callback)
+    }).delete((req, res) => {
+        // var donorId = { "donorId": req.params.id }
+        var donorId = req.params.id
+        var query = {
+            "hospitalId": req.body.hospitalId,
+            "bloodGroup": req.body.bloodGroup,
+            "typeOfRequirement": req.body.typeOfRequirement,
+            "patientId": req.body.patientId,
+            "contactNo": req.body.contactNo
+        }
+        var callback = (err) => {
+            if (err) {
+                res.send({ "Status": false, "Error": err })
+            } else {
+                res.send({ "Status": true })
+            }
+        }
+        Requirement.deLinkDonor(query, donorId, callback)
+
+    })
 module.exports = router
